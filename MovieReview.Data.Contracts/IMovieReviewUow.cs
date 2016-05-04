ca790a075168649def8772472677ad01202b17ee
Arturo Ferreiro <a.ferreirov@hotmail.com>
2016-05-04 09:49:35 -0500

    first commit

---
+++ MovieReview.Data.Contracts/IMovieReviewUow.cs
@@ -0,0 +1,19 @@
+﻿using MovieReview.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieReview.Data.Contracts
+{
+    /// <summary>
+    /// Interface for UOW (Unit Of Work Pattern) Movie Review
+    /// </summary>
+    public interface IMovieReviewUow
+    {
+        void Commit();
+        IRepository<Movie> Movies { get; }
+        IRepository<MoviesReview> MovieReviews { get; }
+    }
+}
---
+++ MovieReview.Model/MoviesReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace MovieReview.Model
+{
+    public class MoviesReview
+    {
+        public int Id { get; set; }
+        public string ReviewerName { get; set; }
+        public string ReviewerComments { get; set; }
+        public int ReviewerRating { get; set; }
+        public int MovieId { get; set; }
+    }
+}
