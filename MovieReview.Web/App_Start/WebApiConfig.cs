a329a1ab415c22f41da86aa1e77d5ed4b2983142
artchess <art_chess@hotmail.com>
2016-05-05 20:14:13 -0500

    Posicion 2095

--- MovieReview.Web/App_Start/WebApiConfig.cs
+++ MovieReview.Web/App_Start/WebApiConfig.cs
@@ -10,20 +10,45 @@ namespace MovieReview.Web
 {
     public static class WebApiConfig
     {
+        public static string ControllerOnly = "ApiControllerOnly";
+        public static string ControllerAndId = "ApiControllerAndIntegerId";
+        public static string ControllerAction = "ApiControllerAction";
+
         public static void Register(HttpConfiguration config)
         {
+            // Aqui doy el formato para regresar JSON y no XML por default
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            // config.SuppressDefaultHostAuthentication();
+            // config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+                name: ControllerOnly,
+                routeTemplate: "api/{controller}"
+            );
+
+            config.Routes.MapHttpRoute(
+                name: ControllerAndId,
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: null, //defaults: new {id = RouteParameter.Optional }
+                constraints: new { id = @"^\d+$" } //id must be all digits
+            );
+
+            config.Routes.MapHttpRoute(
+                name: ControllerAction,
+                routeTemplate: "api/{controller}/{action}"
             );
         }
     }
