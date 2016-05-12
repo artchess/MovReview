c79a39e33c3d217d889a2072d34c9508652e45c7
artchess <art_chess@hotmail.com>
2016-05-11 19:04:29 -0500

    posicion 5080

--- MovieReview.Web/js/homeindex.js
+++ MovieReview.Web/js/homeindex.js
@@ -1,5 +1,5 @@
 ﻿
-var module = angular.module("homeIndex", ['ngRoute']);
+var module = angular.module("homeIndex", ['ngRoute', 'ui.bootstrap', 'movieReviewEdit']);
 
 //Route Configuration
 module.config(["$routeProvider", function ($routeProvider) {
@@ -22,31 +22,110 @@ module.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.otherwise({ redirectTo: "/" });
 }]);
 
-module.controller('homeIndexController', ['$scope', '$http', function ($scope, $http) {
+// mi servicio para CRUD
+// $q es un parametro especial que nos permite crear un objeto deferrable
+// un objeto deferrable puede exponer una promesa, de modo que el consumidor de la función _getMovies
+// pueda usarla como sintaxis de lo que es necesario hacer cuando es satisfactorio o hubo algun fallo.
+module.factory('movieService', ['$http', '$q', function ($http, $q) {
 
-    $("#loader").show();
+    //Funcion para traer peliculas
+    var _getMovies = function () {
+        //para resolver la promesa
+        var deferred = $q.defer();
 
-    //Vacio colleccion
-    $scope.data = [];
-
-    //API call
-    //Tomeout para mostrar spinner
-    setTimeout(function () {
+        //API call
         $http.get('/api/movies')
             .then(function (result) {
                 //success
-                //copia la coleccion de result.data a $scope.data
-                angular.copy(result.data, $scope.data);
+                deferred.resolve(result.data);
             }, function () {
                 //Error
-                //Todo: se cambiara luego por toastr lib
-                console.log("No se pudo obtener datos");
-            })
-            .then(function () {
-                $('#loader').hide();
+                deferred.reject();
             });
-    }, 100);
 
+        return deferred.promise;
+    } // fin de _getMovies
+
+    //obtener pelicula por id
+    var _getMovieById = function(Id) {
+        var deferred = $q.defer();
+
+        $http.get('/api/movies/' + Id)
+            .then(function (result) {
+                //Success
+                deferred.resolve(result.data);
+            }, function () {
+                //error
+                deferred.reject();
+            });
+
+        return deferred.promise;s
+    } // fin de _getMovieById
+
+    //edicion de pelicula
+    var _movieEdit = function (movie) {
+        var deferred = $q.defer();
+
+        $http.put('/api/movies/', movie)
+            .then(function () {
+                //Success
+                deferred.resolve();
+            }, function () {
+                //error
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    } // fin de _movieEdit
+
+    // eliminacion de pelicula
+    var _removeMovie = function (Id) {
+        var deferred = $q.defer();
+
+        $http.delete('/api/movies/' + Id)
+            .then(function () {
+                //success
+                deferred.resolve();
+            }, function () {
+                //error
+                deferred.reject();
+            });
+
+        return deferred.promise;
+    }
+
+    //habilito el uso de las siguientes propiedades para que otras partes de angular lo puedan usar
+    return {
+        getMovies: _getMovies,
+        getMovieById: _getMovieById,
+        movieEdit: _movieEdit,
+        removeMovie: _removeMovie
+    };
+}]);
+
+module.controller('homeIndexController', ['$scope', '$http', 'movieService', function ($scope, $http, movieService) {
+    //Coleccion vacía
+    $scope.data = {};
+    $scope.data.movies = [];
+
+    $("#loader").show();
+
+    setTimeout(function () {
+        movieService.getMovies()
+                .then(function (data) {
+                    //success
+                    //copia la coleccion de result.data a $scope.data
+                    angular.copy(data, $scope.data.movies);
+                    toastr.success('Obtención de películas satisfactoria.');
+                },
+                function () {
+                    //error
+                    toastr.error('Ocurrio un error al obtener las películas');
+                })
+                .then(function(){
+                    $('#loader').hide();
+                });
+    }, 10);
 }]);
 
 module.controller('newMovieController', ["$scope", "$http", "$window", function ($scope, $http, $window) {
@@ -94,4 +173,47 @@ module.controller('newMovieController', ["$scope", "$http", "$window", function 
     //            });
     //    }
     //}, 1000);
+}]);
+
+module.controller('reviewsController', ["$scope", "$routeParams", "$window", "movieService", function ($scope, $routeParams, $window, movieService) {
+
+        $scope.reviews = null;
+        $scope.MovieId = null;
+
+        //$scope.goToAddReview = function () {
+        //    $window.location = "/#newReview/" + $routeParams.Id;
+        //}
+        ////Setting Timeout for spinner
+        //$('#loader').show();
+        ////Timeout function to show spinner
+        //setTimeout(function () {
+        //    dataService.getReviewById($routeParams.Id)
+        //        .then(function (review) {
+        //            //Success
+        //            //For pagination
+        //            $scope.currentPage = 1;
+        //            $scope.numPerPage = 10;
+        //            $scope.maxSize = 11;
+
+        //            $scope.numPages = function () {
+        //                return Math.ceil(review.length / $scope.numPerPage);
+        //            };
+
+        //            $scope.$watch('currentPage + numPerPage', function () {
+        //                var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+        //                , end = begin + $scope.numPerPage;
+        //                $scope.filteredReviews = review.slice(begin, end);
+
+        //            });
+        //            $scope.reviews = review;
+        //            $scope.MovieId = $routeParams.Id;
+        //            toastr.success("Reviews retrieved Successfully");
+        //        }, function () {
+        //            //Error
+        //            toastr.error("Error in Fetching Reviews");
+        //        })
+        //        .then(function () {
+        //            $('#loader').hide();
+        //        });
+        //}, 1000);
 }]);
---
+++ MovieReview.Web/js/movie-review-edit.js
@@ -0,0 +1,58 @@
+﻿
+//Defined Module
+var module = angular.module('movieReviewEdit', []);
+
+//Rutas Definidas
+module.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when('/editMovie/:Id', {
+        controller: 'movieEditController',
+        templateUrl: '/templates/editMovie.html'
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/movies'
+    });
+
+}]);
+
+module.controller('movieEditController', ['$scope', 'movieService', '$window', '$routeParams', function ($scope, movieService, $window, $routeParams) {
+    //inicializo movie y movie id
+    $scope.movie = null;
+    $scope.MovieId = null;
+
+    //obtengo la pelicula por id
+    movieService.getMovieById($routeParams.Id)
+            .then(function (data) {
+                //success
+                $scope.movie = data;
+            }, function () {
+                //error
+                toastr.error('Error al obtener pelicula con id:' + $routeParams.Id);
+            });
+
+    $scope.editMovie = function () {
+        movieService.movieEdit($scope.movie)
+            .then(function () {
+                //success
+                toastr.success('¡Pelicula <b>editada</b> satisfactoriamente!');
+                $window.location = '#/movies';
+            }, function () {
+                //error
+                toastr.error('Error al editar pelicula');
+            });
+    };
+
+    $scope.deleteMovie = function () {
+        movieService.removeMovie($scope.movie.Id)
+            .then(function () {
+                //success
+                toastr.success('¡Pelicula <b>eliminada</b> satisfactoriamente!');
+                $window.location = '#/movies';
+            }, function () {
+                //error
+                toastr.error('Error al eliminar película con el Id: ' + $scope.movie.Id);
+            });
+    };
+
+}]);
